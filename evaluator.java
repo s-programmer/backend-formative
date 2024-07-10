a6855eb47b144ace07138c934f16cba1b1a85547
ignace62 <159723371+ignace62@users.noreply.github.com>
2024-07-10 12:06:02 +0200

    Add files via upload

---
+++ evaluator.java
@@ -0,0 +1,41 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/JSP_Servlet/Servlet.java to edit this template
+ */
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import jakarta.servlet.http.HttpSession;
+
+
+public class evaluator extends HttpServlet {
+
+  
+    
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+             response.setContentType("text/html"); 
+PrintWriter out=response.getWriter(); 
+
+ HttpSession session=request.getSession(false); 
+ if(session!=null&& "username".equals("evaluator123")){ 
+ String name=(String)session.getAttribute("username"); 
+ out.print("Hello, "+name+" Welcome to Profile");
+ request.getRequestDispatcher("student.html").include(request, response);
+ } 
+ else{ 
+   out.print("<script>alert('Please login first')</script>"); 
+  request.getRequestDispatcher("index.html").include(request, response);
+ } 
+out.close(); 
+    }
+
+    }
+
+   
+
